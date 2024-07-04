c5a30e38673708eddfba7cc28c1b2790a73aed2d
abdirahmanmuktar <abdirahmanmuktar18@gmail.com>
2024-07-04 10:01:21 +0700

    created function.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+var pickupLocation = 55;//pick location var
+var headQuater = 45;//head quater street/block
+
+//calculate distance of blocks between pickuplocation and headQuater
+
+function distanceFromHqInBlocks(pickupLocation) {
+    //returns the number of blocks given a value
+    var blocks  = pickupLocation - headQuater;
+    return blocks;
+
+  }
+
+  console.log(distanceFromHqInBlocks(55));
+
+  
+
+  //func for feet
+  function distanceFromHqInFeet() {
+    distanceFromHqInBlocks(blocks);
+    let distanceInFeet = blocks * 264;
+    return distanceInFeet;
+    // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+    // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+  
+    // the return value of distanceFromHqInBlocks can then be used to calculate feet
+  }
+
+  console.log(distanceFromHqInFeet());
